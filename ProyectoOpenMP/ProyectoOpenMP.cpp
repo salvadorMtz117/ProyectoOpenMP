d1bb45b530a43f71d06d8931b166ba7c653695fe
Salvador Martinez <122409728+salvadorMtz117@users.noreply.github.com>
2024-01-24 23:06:02 -0600

    First commit

---
+++ ProyectoOpenMP/ProyectoOpenMP.cpp
@@ -0,0 +1,55 @@
+// ProyectoOpenMP.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <omp.h>
+
+#define N 1000
+#define chunk 100
+#define mostrar 10
+
+void imprimeArreglo(float *d);
+
+int main()
+{
+    std::cout << "Sumando arrelgos en Paralelo!\n";
+    float a[N], b[N], c[N];
+    int i;
+
+    for (i = 0; i < N; i++) {
+        a[i] = i * 10;
+        b[i] = (i + 3) * 3.7;
+    }
+
+    int pedazos = chunk;
+
+    #pragma omp parallel for \
+    shared(a, b, c, pedazos) private(i) \
+    schedule(static, pedazos)
+
+    for (i = 0; i < N; i++)
+        c[i] = a[i] + b[i];
+
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
+    imprimeArreglo(a);
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
+    imprimeArreglo(b);
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << std::endl;
+    imprimeArreglo(c);
+}
+
+void imprimeArreglo(float* d) {
+    for (int x = 0; x < mostrar; x++)
+        std::cout << d[x] << " - ";
+    std::cout << std::endl;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Actividad 1, Programación paralela
+
+Módulo 2. Modelos de programación (Semana 3)
+
+Suma de dos areglos a través de programación paralela
+
+
+## Authors
+Salvador Martínez - A01273366
+- [@salvadorMtz117](https://github.com/salvadorMtz117)
+
